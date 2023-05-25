52ad350a9e25239f357c8a4a95ef436d28afdfad
Victoria McKenzie <vmmc@student.unimelb.edu.au>
2023-05-25 12:43:55 +1000

    before cart add

--- CODE FILES/JS/script.js
+++ CODE FILES/JS/script.js
@@ -1,18 +1,25 @@
-function validateForm() {
-    let registeredNames = ["alice", "bob", "carol", "dave"];
-    let inputName = document.querySelector('#name').value;
-    if (registeredNames.indexOf(inputName) == -1){
-        alert('name is not in our list');
-        return false
-    }
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-function validateForm() {
-    let inputDate = document.querySelector('#when').value;
-    let parsedDate = Date.parse(inputDate);
-    if (parsedDate <= Date.now() ) {
-        alert("Please choose a future date for delivery")
-        //dont do alert - make new element if you want to slay
-        return false
-    }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
